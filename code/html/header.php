5b554f7ebea37793224c523ba561d5eaeec35d88
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2023-06-08 13:48:19 +0200

    styles toegevoegd

--- code/html/header.php
+++ code/html/header.php
@@ -3,7 +3,10 @@
     <head>
         <meta http-equiv="content-type" content="text/html;charset=UTF-8">
         <title>corleone calzone pizza</title>
-        <link href="css/style.css" rel="stylesheet">
+        <link href="../css/style.css" rel="stylesheet">
+        <link href="../css/styleAnouk.css" rel="stylesheet">
+        <link href="../css/styleMax.css" rel="stylesheet">
+        <link href="../css/styleRoan.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body>
