0894ad8c7f7c06a026e806d8ddfc7b5c68765f30
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2023-06-16 12:01:03 +0200

    x

--- code/html/home.php
+++ code/html/home.php
@@ -3,7 +3,20 @@ include("head.php");
 include("header.php");
 ?>
 
+<div id="gridProduct">
+    <div class="productBox">
 
+    </div>
+    <div class="productBox">
+
+    </div>
+    <div class="productBox">
+
+    </div>
+    <div class="productBox">
+
+    </div>
+</div>
 <?php
 include("footer.php");
 ?>
