48032964136cf9f77a26cb6ef04fe559c1e7d9c0
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2023-06-19 12:04:53 +0200

    .

--- code/html/home.php
+++ code/html/home.php
@@ -5,7 +5,15 @@ include("header.php");
 
 <div id="gridProduct">
     <div class="productBox">
+        <img src="../images/pizzaHawaii.jpg" class="pizzaImage">
+        <h3 class="pizzaName">hawaii</h3>
+        <div class="productDescriptionBox">
+            <p class="productDescription">Lorem ipsum dolor sit amet. Qui quia vitae aut perferendis voluptatem rem odio distinctio qui galisum vitae sit illum cumque aut voluptatem veniam. Sed internos dolores ut odit pariatur qui sunt molestiae et modi voluptates sit voluptatum autem aut perferendis soluta aut modi incidunt! Hic dolorem dolorem ut nihil aperiam ut perspiciatis omnis ab provident facere sit corrupti ducimus. Vel corrupti blanditiis vel necessitatibus quis eum consequatur numquam et mollitia omnis.
 
+            Quo adipisci reprehenderit aut omnis facilis et quia pariatur ad recusandae alias ea nihil magnam id veritatis quia ut nulla labore. Ut consequuntur laboriosam aut natus consequatur vel commodi nobis At debitis omnis. Ut animi iusto a libero consectetur est voluptatem magni!
+            </p>
+        </div>
+        <p>price</p>
     </div>
     <div class="productBox">
 
