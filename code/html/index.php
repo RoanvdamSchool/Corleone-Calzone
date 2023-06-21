6be9193ee3c32e3e5ed4442e2e6ad6a0a6216efe
maxingtoni <6027913@mborijnland.NL>
2023-06-21 13:36:06 +0200

    max css

--- code/html/index.php
+++ code/html/index.php
@@ -1,4 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login Form</title>
+</head>
+<body>
+  <h2>Login Form</h2>
+  
+  <div><form>
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br><br>
+    
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+    
+    <input type="submit" value="Submit">
+  </form>
+  
+</body>
+</html>
+
 <?php
+    
     include("head.php");
     include("header.php");
 ?>
