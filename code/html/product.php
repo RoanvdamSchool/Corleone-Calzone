61168059c6fe5d6dac4f3e6e6d7ccabbbdb1d8ef
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2023-06-21 13:43:50 +0200

    product

--- code/html/product.php
+++ code/html/product.php
@@ -1,31 +1,30 @@
 <?php
 include("head.php");
 include("header.php");
+include("../php/databaseFunctions.php");
 ?>
 <div id="buyProductBox" class="col-10 offset-1">
-    <section id="buyProductImage">
-        <img src="../images/pizzaHawaii.jpg" class="buyProductImage">
-    </section>
-    <section class="buyProductInfo">
-        <h2 class="buyPizzaName">pizza hawaii</h2>
-            <form action="home.php">
-                <section id="chooseSize">
-                    <h4>kies grootte</h4>
-                    <input type="radio" name="medium" value="medium">
-                    <label for="medium">medium</label><br>
-                    <input type="radio" name="large" value="large">
-                    <label for="large">large</label><br>
-                    <input type="radio" name="calzone" value="calzone">
-                    <label for="calzone">calzone</label>
-                </section>
-                <section id="amount">
-                    <label for="amount">hoeveelheid</label><br>
-                    <input type="number" name="amount" class="inputField">
-                </section>
-                <input type="submit" value="zet in winkelwagen" id="submitButton">
-            </form>
-    </section>
+    <?php
+    showProduct();
+    ?>
 </div>
+<script>
+    const radioButtons = document.querySelectorAll('input[type="radio"][name="size"]');
+    let selectedValue = null;
+
+    radioButtons.forEach((radioButton) => {
+        radioButton.addEventListener('change', (event) => {
+            if (radioButton.checked) {
+                selectedValue = radioButton.value;
+                console.log('Selected value:', selectedValue);
+                
+                if (selectedValue == "medium") {
+                    console.log(5.95);
+                }
+            }
+        });
+    });
+</script>
 <?php
 include("bottom.php");
 ?>
