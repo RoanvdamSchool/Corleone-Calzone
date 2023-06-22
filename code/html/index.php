122db8bdf2b9359545f68bdace5ee7d854cdfc3b
maxingtoni <6027913@mborijnland.NL>
2023-06-22 14:03:49 +0200

    submit

--- code/html/index.php
+++ code/html/index.php
@@ -1,20 +1,22 @@
-<div class="pizzaBackground"></div>
+
 <?php
     
     include("headerLogin.php");
 ?>
+<div class="pizzaBackground"></div>
+<div class="username">
+  <h2>Login Form</h2>
 
-<div class="username"><h2>Login Form</h2>
-  
-<form>
-  <label for="username">Username:</label><br>
-  <input type="text" id="username" name="username"><br><br>
-  
-  <label for="password">Password:</label><br>
-  <input type="password" id="password" name="password"><br><br>
-  
-  <input type="submit" value="Submit">
-</form></div>
+  <form action="home.php" method="POST">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br><br>
+
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+
+    <input type="submit" value="Submit">
+  </form>
+</div>
 
 <?php
     include("bottom.php");
