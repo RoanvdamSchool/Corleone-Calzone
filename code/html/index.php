ec5f4fe1f1c76c1428f7e769865cc1b760087dac
maxingtoni <6027913@mborijnland.NL>
2023-06-22 13:21:29 +0200

    oplossing

--- code/html/index.php
+++ code/html/index.php
@@ -1,32 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Login Form</title>
-</head>
-<body>
-<link href="../css/styleMax.css" rel="stylesheet">
-<div class="username"><h2>Login Form</h2>
-  
-  <form>
-    <label for="username">Username:</label><br>
-    <input type="text" id="username" name="username"><br><br>
-    
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password"><br><br>
-    
-    <input type="submit" value="Submit">
-  </form></div>
-  
-</body>
-</html>
 
 <?php
     
-    include("head.php");
-    include("header.php");
+    include("headerLogin.php");
 ?>
-
-
+<div class="pizzaBackground">
+<div class="username"><h2>Login Form</h2>
+  
+<form>
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username"><br><br>
+  
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password"><br><br>
+  
+  <input type="submit" value="Submit">
+</form></div>
+</div>
 <?php
     include("bottom.php");
 ?>
