fcc6b14864329348480f5221c4618911b400271b
anenouk <agrandia04@gmail.com>
2023-06-23 12:01:37 +0200

    j

---
+++ code/html/admin.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/style.css?v=<?php echo time(); ?>" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/styleAnouk.css?v=<?php echo time(); ?>">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<header>
+    <h1 id="websiteName">Corleone Calzone Pizza</h1>
+    <nav>
+        <a href="">voeg product toe</a>
+        <a href="">voeg ingrediënt toe</a>
+        <a href="">bestellingen</a>
+    </nav>
+    <img id="websiteLogo" src="../images/carleone%20calzone_LOGO.png" alt="websiteLogo">
+</header>
+
+<main>
+    <div class="container">
+        <div class="ananas">
+            <img id="ananasStukjes" src="../images/ananasStukjes.jpg" alt="ananasStukjes">
+            <p id="naam">ananas stukjes</p>
+            <p id="pst">p.st:€0,50</p>
+            <div class="ananasInfo">
+                <p>yammie yammie,overheerlijke stukjes ananas voor op jouw...</p>
+            </div>
+            <a href="">pas aan</a>
+            <a href="">verwijder</a>
+            <p>aantal</p>
+        </div>
+        <div class="hawaii">
+            <img id="hawaii" src="../images/pizzaHawaii.jpg" alt="hawaii">
+        </div>
+        <div class="ananas">
+        <img id="ananasStukjes" src="../images/ananasStukjes.jpg" alt="ananasStukjes">
+            <p id="naam">ananas stukjes</p>
+            <p>p.st:€0,50</p>
+            <div class="ananasInfo">
+                <p>yammie yammie,overheerlijke stukjes ananas voor op jouw...</p>
+            </div>
+            <a href="">pas aan</a>
+            <a href="">verwijder</a>
+            <p>aantal</p>        </div>
+        <div class="hawaii">
+            <img id="hawaii" src="../images/pizzaHawaii.jpg" alt="hawaii">
+        </div>
+    </div>
+</main>
--- code/html/head.php
+++ code/html/head.php
@@ -6,7 +6,7 @@
         <title>corleone calzone pizza</title>
         <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
         <link href="../css/style.css" rel="stylesheet">
-        <link href="../css/styleAnouk.css" rel="stylesheet">
+        <link href="../css/styleAnouk.css?v=<?php echo time(); ?>" rel="stylesheet">
         <link href="../css/styleMax.css" rel="stylesheet">
         <link href="../css/styleRoan.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
